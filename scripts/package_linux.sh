8156d736c30fbbd712ccae562245ac68ace9e112
Kutlwano P. Maruatona <kutlwanodrew.dev@gmail.com>
2026-09-25 16:04:14 +0200

    fix: update snapcraft.yaml to use dump plugin from build bundle

--- scripts/package_linux.sh
+++ scripts/package_linux.sh
@@ -99,5 +99,5 @@ ARCH=x86_64 ./appimagetool --no-appstream "$APP_DIR" "$DIST_DIR/${APP_NAME}-${RE
 # 5. Snap (.snap via Snapcraft)
 # ---------------------------------------------------------
 echo "Packaging Snap..."
-snapcraft --target-arch=amd64
+snapcraft pack
 mv *.snap "$DIST_DIR/"
